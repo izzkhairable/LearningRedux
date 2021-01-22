f8bba8726bdb18c0e2d93c991d066da7ea1af84c
izzkhairable <izzkhair@gmail.com>
2021-01-22 16:33:35 +0800

    Plus Minus Function w Redux

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,11 +1,35 @@
-
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
-  return (
-    <div className="App">
-      Testing, Check Console.log()
-    </div>
-  );
+	const counter = useSelector((state) => state.counter);
+	const isLogged = useSelector((state) => state.isLogged);
+	const dispatch = useDispatch();
+
+	return (
+		<div className="App">
+			<h1>Counter {counter}</h1>
+			<button
+				onClick={() => {
+					dispatch(increment(5));
+				}}
+			>
+				+
+			</button>
+
+			<button
+				onClick={() => {
+					dispatch(decrement());
+				}}
+			>
+				-
+			</button>
+			{isLogged && (
+				<h3>Valuable information i shouldn't see if not logged in</h3>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -1,47 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from 'redux';
+import { createStore } from 'redux';
+import allReducer from './reducers/index';
+import './index.css';
+import { Provider } from 'react-redux';
 
-
-//STORE -> GLOBALIZED STATE
-
-//ACTION INCREMENT
-const increment =()=>{
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-//ACTION DECREMENT
-const decrement =()=>{
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-//REDUCER
-const counter=(state=0, action)=>{
-  // eslint-disable-next-line default-case
-  switch(action.type){
-    case "INCREMENT":
-      return state+1;
-    case "DECREMENT":
-      return state-1;
-  }
-}
-
-let store=createStore(counter)
-store.subscribe(()=>console.log(store.getState()))
-
-//DISPATCH
-store.dispatch(increment())
-store.dispatch(decrement())
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+	allReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
